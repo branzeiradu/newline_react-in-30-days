b493ecdcb5273fda6f2f4b17303417aaa74c9164
Radu <branzeiradu@gmail.com>
2023-10-21 21:30:03 +0300

    Day 17: React router.

--- 30days/src/App.js
+++ 30days/src/App.js
@@ -1,19 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-import React, { Component } from "react";
-import "whatwg-fetch";
-import Header from "./components/Header";
-import Content from "./components/Content";
+const Home = () => (
+  <div>
+    <h2>Welcome home</h2>
+    <Link to="/about">Go to about</Link>
+  </div>
+);
 
-class App extends Component {
+
+/*
+const About = () => (
+  <div>
+    <h2> About </h2>
+    <Link to="/">Go home</Link>
+  </div>
+);
+*/
+
+const About = ({ name }) => (
+  <div>
+    <h1>About {name}</h1>
+  </div>
+);
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Content />
-      </div>
-    )
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/" exact={true} render={
+              renderProps => (
+                <div>
+                  <Home {...this.props} {...renderProps} />
+                </div>
+              )
+            } />
+
+
+            <Route path="/about" render={renderProps => (
+              <div>
+                <Link to="/about/ari">Ari</Link>
+                <Link to="/about/nate">Nate</Link>
+                <About name={renderProps.match.params.name} />
+                <Link to="/">Go home</Link>
+              </div>
+            )} />
+
+
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
 
--- 30days/src/index.js
+++ 30days/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
+import App from './App.js';
 //import App from './day-13/App.js';
-import App from './day-15/App.js';
+// import App from './day-15/App.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
